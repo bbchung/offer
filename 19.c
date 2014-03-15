57a2fc6f1248f7691ac25e7e9379f4b67364d48b
bb <afafaf4@gmail.com>
2014-03-15 22:45:23 +0800

    19

---
+++ 19.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+#include "node.h"
+
+void MirrorTree(TreeNode* pRoot)
+{
+    if (pRoot == NULL)
+        return;
+
+    TreeNode* temp = pRoot->left;
+    pRoot->left = pRoot->right;
+    pRoot->right = temp;
+
+    MirrorTree(pRoot->left);
+    MirrorTree(pRoot->right);
+}
+
+int main()
+{
+}
+
