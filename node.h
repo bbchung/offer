6ba7f141be6a974173f9bbfecca635f7478a9f09
bb <afafaf4@gmail.com>
2014-03-15 22:10:42 +0800

    node.h

---
+++ node.h
@@ -0,0 +1,13 @@
+typedef struct _TreeNode
+{
+    int value;
+    struct _TreeNode* left;
+    struct _TreeNode* right;
+} TreeNode;
+
+typedef struct _ListNode
+{
+    int value;
+    struct _ListNode* next;
+} ListNode;
+
