f75c1ae737a30937b87c523d023e9e7ea44d9f78
bb <afafaf4@gmail.com>
2014-03-27 15:44:23 +0800

    35-3

---
+++ 35-3.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <string.h>
+
+void RemoveDup(char* str)
+{
+    if (str == NULL)
+        return;
+
+    int len =0;
+    int i = 0;
+    while (str[i] != '\0')
+        ++i;
+
+    len = i;
+
+    char hash[256] = {0};
+    for (i=0;i<len;++i)
+        hash[str[i]]++;
+
+    char* p1 = str + len - 1;
+    char* p2 = NULL;
+
+    while (p1 >= str)
+    {
+        if (hash[*p1] > 1)
+        {
+            if (p2 == NULL)
+                p2 = p1 + 1;
+
+            hash[*p1]--;
+        }
+        else
+        {
+            if (p2 != NULL)
+            {
+                strcpy(p1+1, p2);
+                p2 = NULL;
+            }
+        }
+
+        p1--;
+    }
+}
+
+int main()
+{
+    char str[] = "ggoogggglleeggoooodd";
+    RemoveDup(str);
+
+    printf("%s\n", str);
+}
