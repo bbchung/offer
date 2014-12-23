96b29450152f13a26b42005ba601f8ea86e2dcba
bbchung <afafaf4@gmail.com>
2014-12-23 17:53:28 +0800

    itoa.c

---
+++ itoa.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int count_digit(int number)
+{
+    int len=0;
+    int q = number;
+    while (1)
+    {
+        len++;
+
+        q /= 10;
+        if (q == 0)
+            break;
+    }
+
+    if (number < 0)
+        len++;
+
+    return len;
+}
+
+char *itoa(int number)
+{
+    int len = count_digit(number) + 1;    
+
+    char *buf = malloc(len);
+    buf[len - 1] = 0;
+    int i = len - 2;
+
+    if (number < 0)
+    {
+        buf[0] = '-';
+        number = -number;
+    }
+
+    while (1) 
+    {
+        buf[i] = number % 10 + '0';
+        number = number/10; 
+        i--;
+        if (number == 0)
+            break;
+
+    }
+
+    return buf;
+}
+
+int main()
+{
+    printf("%s\n", itoa(100));
+}
