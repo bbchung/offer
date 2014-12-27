2c345ac486e600a8987300173adb850f3d349c49
bbchung <afafaf4@gmail.com>
2014-12-27 15:33:07 +0800

    update

--- cipher.c
+++ cipher.c
@@ -13,26 +13,26 @@ int main()
     scanf("%d", &N);
     scanf("%d", &K);
 
-    char C[N + K];
-    scanf("%s", C);
+    char CT[N + K];
+    scanf("%s", CT);
 
-    char P[N + 1];
-    memset(P, 0, N+1);
+    char PT[N + 1];
+    memset(PT, 0, N+1);
 
-    P[0] = TOBYTE(C[0]);
+    PT[0] = TOBYTE(CT[0]);
 
     for (int i=1;i<N;++i)
     {
-        P[i] = TOBYTE(C[i]) ^ TOBYTE(C[i-1]);
+        PT[i] = TOBYTE(CT[i]) ^ TOBYTE(CT[i-1]);
 
         if (i >= K)
-            P[i] ^= P[i-K];
+            PT[i] ^= PT[i-K];
     }
 
     for (int i=0;i<N;++i)
-        P[i] += '0';
+        PT[i] += '0';
 
-    printf("%s\n", P);
+    printf("%s\n", PT);
 
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */
     return 0;
