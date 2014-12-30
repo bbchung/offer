a5eac3fab7c483d76a85fa60755fc1c6e9c0bf92
bbchung <afafaf4@gmail.com>
2014-12-30 20:03:58 +0800

    update

--- hackerrank/subsequence_weighting.c
+++ hackerrank/subsequence_weighting.c
@@ -1,1 +1,51 @@
-#include <>
+#include <stdio.h>
+
+int weight_recursive(int N, int *A, int *W, int head, int min)
+{
+    if (head >= N || head < 0)
+        return 0;
+
+    int next_head = -1;
+    for (int i=head+1;i<N;++i)
+    {
+        if (A[i] > A[head])
+        {
+            next_head = i;
+            break;
+        }
+    }
+
+    int with_head = (A[head] > min) ? W[head] + weight_recursive(N, A, W, next_head, A[head]) : 0;
+    int without_head = weight_recursive(N, A, W, head+1, min);
+
+    if (with_head > without_head)
+        return with_head;
+    else
+        return without_head;
+}
+
+int main()
+{
+
+    int T;
+    scanf("%d", &T); 
+
+    for (int i = 0; i < T; ++i)
+    {
+        int N;
+        scanf("%d", &N); 
+
+        int A[N];
+        for (int j = 0; j < N; ++j)
+            scanf("%d", A + j);
+
+        int W[N];
+        for (int j = 0; j < N; ++j)
+            scanf("%d", W + j);
+
+        int w = weight_recursive(N, A, W, 0, 0);
+
+        printf("%d\n", w);
+    }
+
+}
--- play_game.c
+++ play_game.c
@@ -30,7 +30,6 @@ int main()
 
         int *A = malloc(N * sizeof(int)); // value
         int *PtoI = malloc(N * sizeof(int)); // point of current row to index
-        int *StoI = malloc(N * sizeof(int)); // sum of current row to index
         int *StoN = malloc(N * sizeof(int)); // sum of index to N
         int *PtoN = malloc(N * sizeof(int)); // point of index to N;
 
@@ -40,46 +39,57 @@ int main()
 
         for (int j = 0; j < N; ++j)
         {
-            printf("j=%d ", j);
             scanf("%d", A + j);
         }
 
         for (int j = N-1; j >= 0; j--)
         {
-            memset(StoI, 0, N * sizeof(int));
             memset(PtoI, 0, N * sizeof(int));
 
-
-            for (int k=j;k<N;++k)
+            if (j == N-1)
             {
-                if (k == j)
-                {
-                    StoI[k] = A[k];
-                }
-                else
-                {
-                    StoI[k] = StoI[k-1] + A[k];
-                }
-                
-                if (k == j)
-                {
-                    PtoI[k] = A[k];
-                }
-                else if (k-j < 3)
-                {
-                    PtoI[k] = PtoI[k-1] + A[k];
-                }
-                else
-                {
-                    int i1 = PtoI[j] + StoN[j+1] - PtoN[j+1];
-                    int i2 = PtoI[j+1] + StoN[j+2] - PtoN[j+2];
-                    int i3 = PtoI[j+2] + StoN[j+3] - PtoN[j+3];
+                StoN[j] = A[j];
+            }
+            else
+            {
+                StoN[j] = StoN[j+1] + A[j];
+            }
 
-                    PtoI[k] = max3(i1, i2, i3);
+            if (N-1-j<3)
+            {
+                for (int k=N-1;k>=j;--k)
+                {
+                    if (k == j+2)
+                    {
+                        PtoN[k] = A[k];
+                    }
+                    else
+                    {
+                        printf("PtoN[%d] = %d\n", k, PtoN[k-1] + A[k]);
+                        PtoN[k] = PtoN[k+1] + A[k];
+                    }
                 }
             }
-            PtoN[j] = PtoI[N-1];
-            StoN[j] = StoI[N-1];
+            else
+            {
+                for (int k=j;k<j+3;++k)
+                {
+                    if (k == j)
+                    {
+                        PtoI[k] = A[k];
+                    }
+                    else if (k-j < 3)
+                    {
+                        PtoI[k] = PtoI[k-1] + A[k];
+                    }
+                }
+
+                int i1 = PtoI[j] + StoN[j+1] - PtoN[j+1];
+                int i2 = PtoI[j+1] + StoN[j+2] - PtoN[j+2];
+                int i3 = PtoI[j+2] + StoN[j+3] - PtoN[j+3];
+
+                PtoN[j] = max3(i1, i2, i3);
+            }
         }
 
         printf("%d\n", PtoN[0]);
