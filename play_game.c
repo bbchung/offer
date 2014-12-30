5b26e7ad89ad19bef4081e7bf9e902f591331738
ubuntu 14.10 <afafaf4@gmail.com>
2014-12-31 00:43:00 +0800

    update play_game.c

--- play_game.c
+++ play_game.c
@@ -4,9 +4,9 @@
 #include <stdlib.h>
 
 
-int max3(int i1, int i2, int i3)
+unsigned long long int max3(unsigned long long int i1, unsigned long long int i2, unsigned long long int i3)
 {
-    int max = i1;
+    unsigned long long int max = i1;
     if (i2 > max)
         max = i2;
 
@@ -18,7 +18,6 @@ int max3(int i1, int i2, int i3)
 
 int main()
 {
-
     int T;
     scanf("%d", &T);
 
@@ -27,25 +26,21 @@ int main()
         int N;
         scanf("%d", &N);
 
+        unsigned long long int *A = malloc(N * sizeof(unsigned long long int)); // value
+        unsigned long long int *StoN = malloc(N * sizeof(unsigned long long int)); // sum of index to N
+        unsigned long long int *PtoN = malloc(N * sizeof(unsigned long long int)); // point of index to N;
 
-        int *A = malloc(N * sizeof(int)); // value
-        int *PtoI = malloc(N * sizeof(int)); // point of current row to index
-        int *StoN = malloc(N * sizeof(int)); // sum of index to N
-        int *PtoN = malloc(N * sizeof(int)); // point of index to N;
-
-        memset(A, 0, N * sizeof(int));
-        memset(PtoN, 0, N * sizeof(int));
-        memset(StoN, 0, N * sizeof(int));
+        memset(A, 0, N * sizeof(unsigned long long int));
+        memset(PtoN, 0, N * sizeof(unsigned long long int));
+        memset(StoN, 0, N * sizeof(unsigned long long int));
 
         for (int j = 0; j < N; ++j)
         {
-            scanf("%d", A + j);
+            scanf("%llu", A + j);
         }
 
         for (int j = N-1; j >= 0; j--)
         {
-            memset(PtoI, 0, N * sizeof(int));
-
             if (j == N-1)
             {
                 StoN[j] = A[j];
@@ -55,44 +50,30 @@ int main()
                 StoN[j] = StoN[j+1] + A[j];
             }
 
-            if (N-1-j<3)
+            if (j == N-1)
             {
-                for (int k=N-1;k>=j;--k)
-                {
-                    if (k == j+2)
-                    {
-                        PtoN[k] = A[k];
-                    }
-                    else
-                    {
-                        printf("PtoN[%d] = %d\n", k, PtoN[k-1] + A[k]);
-                        PtoN[k] = PtoN[k+1] + A[k];
-                    }
-                }
+                PtoN[j] = A[j];
+            }
+            else if (j == N-2 || j == N-3)
+            {
+                PtoN[j] = PtoN[j+1] + A[j];
             }
             else
             {
-                for (int k=j;k<j+3;++k)
-                {
-                    if (k == j)
-                    {
-                        PtoI[k] = A[k];
-                    }
-                    else if (k-j < 3)
-                    {
-                        PtoI[k] = PtoI[k-1] + A[k];
-                    }
-                }
 
-                int i1 = PtoI[j] + StoN[j+1] - PtoN[j+1];
-                int i2 = PtoI[j+1] + StoN[j+2] - PtoN[j+2];
-                int i3 = PtoI[j+2] + StoN[j+3] - PtoN[j+3];
+                unsigned long long int p0 = A[j];
+                unsigned long long int p1 = p0 + A[j+1];
+                unsigned long long int p2 = p1 + A[j+2];
+
+                unsigned long long int i1 = p0 + StoN[j+1] - PtoN[j+1];
+                unsigned long long int i2 = p1 + StoN[j+2] - PtoN[j+2];
+                unsigned long long int i3 = p2 + StoN[j+3] - PtoN[j+3];
 
                 PtoN[j] = max3(i1, i2, i3);
             }
         }
 
-        printf("%d\n", PtoN[0]);
+        printf("%llu\n", PtoN[0]);
     }
 
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */
