5b3ce35b091603087e75f740e912b4bad0dbad99
bbchung <afafaf4@gmail.com>
2015-01-05 16:34:54 +0800

    update

---
+++ change_coin.cpp
@@ -0,0 +1,38 @@
+#include <string.h>
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+int M[4] = {2, 3, 5, 6};
+int c[1000 + 1];
+
+void change(int m) // 看看 {5, 2, 6, 11, 17} 湊得價位 m 的湊法有幾種
+{
+    memset(c, 0, sizeof(c)); // 預設為湊不到，每一格都為 0。
+    c[0] = 1;                // 預設價位為 0 時一定湊得到
+
+    for (int i = 0; i < 4; ++i)
+        for (int j = M[i]; j <= m; ++j)
+        {
+            c[j] += c[j - M[i]]; // 湊、湊、湊
+            if (j == 5)
+            {
+                printf("+=c[%d](%d)\n", j - M[i], c[j - M[i]]);
+            }
+        }
+
+    //cout << "湊得價位" << m
+         //<< "的湊法有" << c[m] << "種" << endl;
+
+    for (int i = 0; i <= 5; ++i)
+    {
+    cout << "湊得價位" << i
+         << "的湊法有" << c[i] << "種" << endl;
+        
+    }
+}
+
+int main()
+{
+    change(5);
+}
--- cutting_boards.c
+++ cutting_boards.c
@@ -2,9 +2,10 @@
 #include <string.h>
 #include <math.h>
 #include <stdlib.h>
+#include <limits.h>
 
-#define MIN(x, y) (x) < (y) ? (x) : (y)
-#define MAX(x, y) (x) > (y) ? (x) : (y)
+#define MIN(x, y) ((x) < (y) ? (x) : (y))
+#define MAX(x, y) ((x) > (y) ? (x) : (y))
 
 int compare(const void *a, const void *b)
 {
@@ -24,28 +25,29 @@ int main()
         --N1;
         --N2;
 
-        int A1[N1], A2[N2];
+        unsigned long long int A1[N1], A2[N2];
 
         for (int j = 0; j < N1; ++j)
-            scanf("%d", A1 + j);
+            scanf("%llu", A1 + j);
 
         for (int j = 0; j < N2; ++j)
-            scanf("%d", A2 + j);
+            scanf("%llu", A2 + j);
 
-        qsort(A1, N1, sizeof(int), compare);
-        qsort(A2, N2, sizeof(int), compare);
+        qsort(A1, N1, sizeof(unsigned long long int), compare);
+        qsort(A2, N2, sizeof(unsigned long long int), compare);
 
-        int S1[N1], S2[N2]; // sum1, sum2
-        memset(S1, 0, N1);
-        memset(S2, 0, N2);
+        unsigned long long int S1[N1], S2[N2]; // sum1, sum2
+        memset(S1, 0, N1 * sizeof(unsigned long long int));
+        memset(S2, 0, N2 * sizeof(unsigned long long int));
 
-        int S[2][MAX(N1, N2)]; // space
-        int C[2]; // count for space
+        unsigned long long int S[2][N1 + N2 - 1]; // space
+        int C[2] = {0};                           // count for space
+        int SI;                                   // space index
 
-        for (int j=0;j<N1+N2-1;j++)
+        int lc; // last count
+        for (int j = 0; j < N1 + N2 - 1; j++)
         {
-            int SI = j%2; // space index
-
+            SI = j % 2;
 
             int m = j, n = 0;
 
@@ -57,42 +59,32 @@ int main()
             else
             {
                 if (j < N1)
-                    S1[j] = S1[j-1] + A1[j];
+                    S1[j] = S1[j - 1] + A1[j];
 
                 if (j < N2)
-                    S2[j] = S2[j-1] + A2[j];
+                    S2[j] = S2[j - 1] + A2[j];
             }
 
             C[SI] = 0;
-            while (m >= 0)
-            {
-                if (m < N1)
-                {
-                    if (m == 0 || n == 0)
-                    {
-                        S[SI][C[SI]] = 2 * MIN(S1[m], S2[n]) + MAX(S1[m], S2[n]);
-                    }
-                    else
-                    {
-                        S[SI][C[SI]] = MIN(A1[m] + ((C[SI] >= C[(j+1)%2]) ? S[(j+1)%2][C[SI]]+S2[n] : 111111111), A2[n] + ((C[SI] > 0) ? S[(j+1)%2][C[SI]-1] : 1111111) + S1[m]);
-                    }
 
-                    C[SI]++;
+
+            while (m >= 0 && n < N2)
+            {
+                if (m < N1 && n >= 0)
+                {
+                    unsigned long long int choice1 = ((C[SI] >= C[(j + 1) % 2])) ? A1[m] + 2 * S2[n] : A1[m] + S[(j + 1) % 2][C[SI]] + S2[n];
+                    unsigned long long int choice2 = (C[SI] == 0) ? A2[n] + 2 * S1[m] : A2[n] + S[(j + 1) % 2][C[SI] - 1] + S1[m];
+                    S[SI][C[SI]] = MIN(choice1, choice2);
+                    lc = C[SI];
                 }
 
+                C[SI]++;
                 m--;
                 n++;
             }
         }
 
-        printf("%d\n", S[(N1+N2-2)%2][C[(N1+N2-2)%2]] % (1000000000 + 7));
-
-
-        /*W(m, n)
-         * 2 * min(S1[m], S2[n]) + max(S1[m], S2[n]), if m == 0 or n == 0
-         * or
-         * min(A1[m] + W(m-1, n) + S2[n], A2[n] + W(m, n-1) + S1[m])
-         */
+        printf("%llu\n", S[SI][lc] % (1000000000 + 7));
     }
 
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */
