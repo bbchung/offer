77db73461d62764308c1604a182ab12bfaadf38a
bbchung <afafaf4@gmail.com>
2015-02-02 17:45:05 +0800

    update

---
+++ Substring_Diff.cpp
@@ -0,0 +1,99 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+#include <queue> 
+
+int diff_len(char *p, char *q, int M)
+{
+    int ans = 0;
+    int len = 0;
+    int diff = 0;
+
+    int i = 0;
+    std::queue<int> queue;
+
+    while (1)
+    {
+        if (p[i] == '\0' || q[i] == '\0')
+            break;
+
+        if (p[i] == q[i])
+        {
+            ++len;
+            if (len >= ans)
+                ans = len;
+        }
+        else
+        {
+            ++diff;
+            queue.push(i);
+            if (diff > M)
+            {
+                len = i - queue.front();
+                queue.pop();
+                --diff;
+            }
+            else
+            {
+                ++len;
+                if (len >= ans)
+                    ans = len;
+            }
+        }
+
+        ++i;
+    }
+
+    return ans;
+}
+
+int main()
+{
+//    diff_len("abc", "bc", 0);
+
+    int T;
+    scanf("%d", &T);
+
+    for (int i = 0; i < T; ++i)
+    {
+        int S;
+        char P[1501] = {0};
+        char Q[1501] = {0};
+
+        scanf("%d", &S);
+        scanf("%s", P);
+        scanf("%s", Q);
+
+        int ans = 0;
+
+        int j=0;
+
+        while (Q[j] != '\0')
+        {
+            int v = diff_len(P, Q+j, S);
+
+            if (v >= ans)
+                ans = v;
+
+            ++j;
+        }
+
+        j=1;
+        while (P[j] != '\0')
+        {
+            int v = diff_len(Q, P+j, S);
+
+            if (v >= ans)
+                ans = v;
+
+            ++j;
+        }
+
+        printf("%d\n", ans);
+    }
+
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    return 0;
+}
---
+++ print_all_subset.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+void print_all_subset(int *A, int len, int *B, int len2, int index, int *c)
+{
+    if (index >= len)
+    {
+        for (int i = 0; i < len2; ++i)
+        {
+            printf("%d ", B[i]);
+        }
+        printf("\n");
+        (*c)++;
+
+        return;
+    }
+    print_all_subset(A, len, B, len2, index+1, c);
+
+    B[len2] = A[index];
+    print_all_subset(A, len, B, len2+1, index+1, c);
+}
+
+
+
+int main()
+{
+    int A[] = {1, 2, 3, 4, 5, 6, 7};
+    int B[7] = {0};
+
+    int c = 0;
+    print_all_subset(A, 7, B, 0, 0, &c);
+
+    printf("%d\n", c);
+}
+
