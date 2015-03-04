acaef0ed3d07ee9f0566e0b838ebf1362220f3a0
bbchung <afafaf4@gmail.com>
2015-03-04 17:49:15 +0800

    add

---
+++ Sherlock_and_Watson.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+{
+    int N, K, Q;
+    scanf("%d", &N);
+    scanf("%d", &K);
+    scanf("%d", &Q);
+
+    int A[N];
+    for (int i = 0; i < N; ++i)
+        scanf("%d", A+i);
+
+    int offset = -K;
+
+    while (offset < 0)
+        offset += N;
+
+    for (int i = 0; i < Q; ++i)
+    {
+        int idx;
+        scanf("%d", &idx);
+
+        idx += offset;
+        idx %= N;
+
+        printf("%d\n", A[idx]);
+    }
+}
